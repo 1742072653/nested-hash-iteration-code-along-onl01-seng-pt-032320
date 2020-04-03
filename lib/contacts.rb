11d72fa04044fcec2253231bd620acd256e607c9
Wenhao Guo <gwh1742072653@gmail.com>
2020-04-03 19:30:58 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
     if person == "Freddy Mercury"
       data.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
-          data.delete("strawberry")
+          data.delete("strawberry") 
         end
       end
     end
